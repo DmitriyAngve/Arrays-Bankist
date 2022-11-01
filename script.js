93542fa16696a4dbb22b9d614a04fba6036f2f63
Dmitriy Angve <kirirch129@gmail.com>
2022-11-01 11:23:29 +0300

    #

--- script.js
+++ script.js
@@ -73,4 +73,62 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+///////////////////////SIMPLE ARRAY METHODS/////////////////////////
+////////////////////////////////////////////////////////////////////
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE (create new array)
+console.log(arr.slice(2)); // ['c', 'd', 'e'] New array
+console.log(arr.slice(2, 4)); // ['c', 'd'] ( end par - start par)
+console.log(arr.slice(-2)); // ['d', 'e'] Last 2 parametrs
+console.log(arr.slice(1, -2)); // ['b', 'c'] Last 2 parametrs
+console.log(arr.slice()); // ['a', 'b', 'c', 'd', 'e'] NEW ARRAY
+
+// SPLICE (mutate original array)
+// console.log(arr.splice(2)); // ['c', 'd', 'e'] splice deleted this 3 elemets from parents array
+// console.log(arr); // ['a', 'b']
+arr.splice(-1);
+console.log(arr); // ['a', 'b', 'c', 'd']
+arr.splice(1, 2); // From pos1 delete two elemets, include pos1 element
+console.log(arr); // ['a', 'd']
+
+// REVERSE (mutate original array)
+arr = ['a', 'b', 'c', 'd', 'e'];
+
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse()); // ['f', 'g', 'h', 'i', 'j']
+console.log(arr2); // ['f', 'g', 'h', 'i', 'j']
+
+// CONCAT (Doesn't mutate original arrays)
+const letters = arr.concat(arr2);
+console.log(letters); // ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'] Doesn't mutate original arrays
+console.log([...arr, ...arr2]); // Doesn't mutate original arrays
+
+// JOIN
+console.log(letters.join('-')); // create srting "a-b-c-d-e-f-g-h-i-j"
+*/
+
+////////////////////////////////////////////////////////////////////
+///////////////////////THE NEW "AT" METHOD//////////////////////////
+////////////////////////////////////////////////////////////////////
+/*
+const arr = [23, 11, 64];
+console.log(arr[0]); // 23
+// Same thing
+console.log(arr.at(0)); // 23
+
+// How we get last element of the array if don't know array length
+console.log(arr[arr.length - 1]); // 64
+console.log(arr.slice(-1)); // [64]
+console.log(arr.slice(-1)[0]); // 64
+console.log(arr.at(-1)); // 64
+console.log(arr.at(-2)); // 11
+
+// At methods works also with strings
+console.log('jonas'.at(-1)); // s
+*/
+////////////////////////////////////////////////////////////////////
+/////////////////////LOOPING ARRAYS: FOREACH////////////////////////
+////////////////////////////////////////////////////////////////////
