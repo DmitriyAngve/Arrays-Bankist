4796bd325b66dc601505273650fff6392b986b2f
Dmitriy Angve <kirirch129@gmail.com>
2022-11-04 17:59:50 +0300

    #

--- script.js
+++ script.js
@@ -682,7 +682,7 @@ console.log(movements); // [3000, 1300, 450, 200, 70, -130, -400, -650]
 ////////////////////////////////////////////////////////////////////
 ///////////////MORE WAYS OF CREATING AND FILLING ARRAYS/////////////
 ////////////////////////////////////////////////////////////////////
-
+/*
 // Ways of create an array
 const arr = [1, 2, 3, 4, 5, 6, 7];
 console.log(new Array(1, 2, 3, 4, 5, 6, 7));
@@ -716,7 +716,7 @@ console.log(z); // [1, 2, 3, 4, 5, 6, 7] (_, i) - underscore parameter - its mea
 
 // Usefull for iteration of NodeList
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // const movementsUI = Array.from(document.querySelectorAll('.movements__value'));
 // console.log(movementsUI); // [div.movements__value, div.movements__value]
@@ -729,3 +729,136 @@ labelBalance.addEventListener('click', function () {
   console.log(movementsUI);
   const movementsUI2 = [...document.querySelectorAll('.movements__value')];
 }); // [1300, 70, -130, -650, 3000, -400, 450, 200] Nodelist is not array, but array like structure
+*/
+
+////////////////////////////////////////////////////////////////////
+////////////////////ARRAY METHODS PRACTICE//////////////////////////
+////////////////////////////////////////////////////////////////////
+/*
+//1. - 4. Removening from nested arrays into one, find all positive values and adding together
+// const bankDepositSum = accounts.map(acc => acc.movements);
+// console.log(bankDepositSum);
+
+//2.
+// All arrays value out of arrays into one array. Remove all values into a parent array! (flat())
+// const bankDepositSum = accounts.map(acc => acc.movements).flat();
+// console.log(bankDepositSum);
+
+//3. Now filter for the deposites for the positive values
+// const bankDepositSum = accounts.flatMap(acc => acc.movements).filter(mov => mov > 0);
+// console.log(bankDepositSum);
+
+//4. Adding them all together
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+console.log(bankDepositSum);
+//----------------------------------------------------------
+
+//5. How many deposits there have been in the bank with at lest 1000$
+// const numDeposites1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov >= 1000).length;
+// console.log(numDeposites1000); // 6
+
+//6. Another solution
+const numDeposites1000 = accounts
+  .flatMap(acc => acc.movements)
+  // .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+  // .reduce((count, cur) => (cur >= 1000 ? count++ : count), 0); // doesn't work
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0); // doesn't work
+console.log(numDeposites1000); // 6
+
+// Prefixed ++ operator
+let a = 10;
+// console.log(a++); // 10 "++" operator still returns the previous value
+// console.log(a); // 11
+console.log(++a); // 11
+console.log(a); //11
+
+//7. Create a new object instead of just a number or just a string
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+console.log(deposits, withdrawals); // 25180 -7340
+
+//8. Creat function to convert any string to a title case
+// this is nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+  const exeptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exeptions.includes(word) ? word : capitalize(word)))
+    .join(' ');
+  return capitalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+*/
+
+////////////////////////////////////////////////////////////////////
+/////////////////////CODING CHALLENGE #4////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1. Add new property while iterating
+dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+console.log(dogs);
+
+// 2. Find object by array element
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(
+  `Sarah's dog eating ${
+    dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+  } `
+);
+
+// 3. Creating two array from array of objects based on condition
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch);
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle);
+
+// 4.
+// Matilda and Alice and Bob's dogs eat too much!" / "Sarah and John and Michael's dogs eat too little!
+console.log(`${ownersEatTooMuch.join(' and ')} dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')} dogs eat too little!`);
+
+// 5. Find with condition
+console.log(dogs.some(dogs => dogs.curFood === dogs.recFood));
+
+// 6.current > (recommended * 0.90) && current < (recommended * 1.10)
+const checkEatingOkay = dog =>
+  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+console.log(dogs.some(checkEatingOkay));
+
+// 7. Create an array containing dogs that are eating an OKAY anount of food
+console.log(dogs.filter(checkEatingOkay));
+
+// 8. Created a swallow copy of array and sort it
+
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
